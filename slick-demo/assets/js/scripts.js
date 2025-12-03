1d96103addcf1f66a7cc25b1ba416405e7120815
Chance Crosson <chancecrosson@gmail.com>
2025-12-03 17:57:30 -0600

    Added slick-demo and added final reflection to index.

---
+++ slick-demo/assets/js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+    $('.fade').slick({
+      centerMode: true,
+      centerPadding: '30px',
+      slidesToShow: 2,
+      dots: true,
+      infinite: true,
+      speed: 500,
+      fade: true,
+      slide: 'div',
+      cssEase: 'linear',
+      autoplay: true,
+      autoplaySpeed: 2000,
+    });
+
+
+});
